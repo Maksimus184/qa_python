a5e6dfc495219fb1c6bc85ac3c213d34f5e6b928
maksimmaks <mm5524030@gmail.com>
2024-12-23 17:07:46 +0300

    My test

--- tests.py
+++ tests.py
@@ -7,7 +7,6 @@ class TestBooksCollector:
         collector.add_new_book('Гордость и предубеждение и зомби')
         collector.add_new_book('Что делать, если ваш кот хочет вас убить')
         assert len(collector.get_books_genre()) == 2
-
     def test_set_book_genre(self):
         collector = BooksCollector()
         collector.add_new_book('Гордость и предубеждение и зомби')
